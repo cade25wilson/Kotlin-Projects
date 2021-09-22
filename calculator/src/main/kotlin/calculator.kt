260d2d89b7b07f78836d0b7bdd634eefa27617cd
Cade Wilson <cade25wilson@gmail.com>
2021-09-21 20:32:18 -0600

    added operaions
    added repeat function and all other operators

--- calculator/src/main/kotlin/calculator.kt
+++ calculator/src/main/kotlin/calculator.kt
@@ -1,12 +1,44 @@
-fun main() {
+import kotlin.system.exitProcess
+
+fun calculate() {
     println("please input a number: ")
-    val num1 = readLine()!!.toInt()
+    var num1 = readLine()!!.toInt()
     println("Please input another number: ")
-    val num2 = readLine()!!.toInt()
+    var num2 = readLine()!!.toInt()
     println("What operation would you like to perform on these numbers?")
     var operation = readLine()
     if (operation == "+"){
         println(num1 + num2)
+    } else if (operation == "-"){
+        println(num1 - num2)
+    } else if (operation == "*") {
+        println(num1 * num2)
+    } else if (operation == "/"){
+        println(num1 / num2)
+    }
+    else{
+        println("Please enter a valid operator.")
+    }
+    println("Would you like to calculate another set of numbers? Yes or no")
+    repeat()
+}
+
+fun repeat() {
+    var calcAgain = readLine()!!.uppercase()
+    if (calcAgain == "YES"){
+        calculate()
+    } else if (calcAgain == "NO") {
+        println("Thank you for using calculator see you again soon")
+        exitProcess(0)
+    }
+    else {
+        println("Please enter yes or no")
+        repeat()
     }
 
+
+}
+
+fun main() {
+    calculate()
 }
